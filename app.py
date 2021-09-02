64695030acefa9a4f30893bf5a388d640bb0e028
tellezHub <89365683+tellezHub@users.noreply.github.com>
2021-09-01 21:52:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,66 @@
-from flask import Flask, request, jsonify
+from flask import Flask, Response, render_template
+import requests
+import json
+import logging
+
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
 
-    # For debugging
-    print(f"got name {name}")
 
-    response = {}
+logging.basicConfig(level=logging.DEBUG)
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+@app.route("/")
+def hello_world():
+       return ("hola a todos")
 
-    # Return the response in json format
-    return jsonify(response)
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+@app.route('/lenguajes')
+def mostrarLenguajes():
+    misLenguajes = ("PHP", "Python", "Java", "C#",
+                    "JavaScript", "Perl", "Ruby", "Rust")
+    return render_template('lenguajes.html', lenguajes=misLenguajes)
 
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route("/get-price/<ticker>")
+def get_price(ticker):
+    url = f"https://query2.finance.yahoo.com/v10/finance/quoteSummary/{ticker}?modules=price%2CsummaryDetail%2CpageViews%2CfinancialsTemplate"
+    headers={'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url,headers=headers)
+    company_info = response.json()
+
+    if response.status_code > 400:
+        app.logger.info(f"Yahoo has problem with ticker: {ticker}.")
+        app.logger.info(f"Yahoo status code: {response.status_code}.")
+        return Response({}, status=404, mimetype='application/json')
+
+    app.logger.info(company_info)
+
+    try:
+        price = company_info['quoteSummary']['result'][0]['price']['regularMarketPrice']['raw']
+        company_name = company_info['quoteSummary']['result'][0]['price']['longName']
+        exchange = company_info['quoteSummary']['result'][0]['price']['exchangeName']
+        currency = company_info['quoteSummary']['result'][0]['price']['currency']
+
+        result = {
+            "price": price,
+            "name": company_name,
+            "exchange": exchange,
+            "currency": currency
+        }
+        app.logger.info(result)
+
+        return Response(json.dumps(result), status=200, mimetype='application/json')
+    except (KeyError, TypeError):
+        return Response({}, status=404, mimetype='application/json')
+    except Exception as e:
+        app.logger.error("Exception occurred", exc_info=True)
+
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run()
+
+
